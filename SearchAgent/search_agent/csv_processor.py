cce73e509884466c68abeb5aef1a32a84e88b3bc
SeungRyeol Baek <102913030+SeungRyeolBaek@users.noreply.github.com>
2024-06-02 22:33:28 +0900

    Add files via upload

--- SearchAgent/search_agent/csv_processor.py
+++ SearchAgent/search_agent/csv_processor.py
@@ -9,7 +9,8 @@ class CSVProcessor:
         data = pd.read_csv(self.input_file)
         data = data.dropna()
         data['combined_info'] = data.apply(
-            lambda row: f"ID: {row['chunk_id']}, Content: {row['text']}. Source: {row['source']}",
+            lambda row: ', '.join([f"{col}: {row[col]}" for col in data.columns]),
             axis=1
         )
         data[['combined_info']].to_csv(self.output_file, index=False)
+
